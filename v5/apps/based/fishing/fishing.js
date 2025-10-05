5fb3373da330956ff4c017a1b958a04af7c42c01
Marak <marak.squires@gmail.com>
2025-10-05 13:25:35 -1000

    Improved UX for fishing workflow / equipped items / sell all #95 Adds total value of fish inventory Adds age of fish

--- v5/apps/based/fishing/fishing.js
+++ v5/apps/based/fishing/fishing.js
@@ -8,6 +8,7 @@ export default class Fishing {
     this.client = fishingClient;
     this.win = null;
     this.html = null;
+    this.equippedItems = [];
     return this;
   }
 
@@ -74,7 +75,7 @@ export default class Fishing {
 
     let favoriteButton = `<button class="fishing-favorite-item" data-inventory-id="${item.id}">Favorite</button>`;
     if (item.favorited === 1) {
-      favoriteButton = `<button class="fishing-unfavorite-item" data-inventory-id="${item.id}">Unfavorite</button>`;
+      favoriteButton = `<button class="fishing-unfavorite-item" data-inventory-id="${item.id}">❤️</button>`;
     }
 
     return `<div class="fishing-item">
@@ -93,20 +94,51 @@ export default class Fishing {
     if (typeof item.item_def === 'string') {
       item.item_def = JSON.parse(item.item_def);
     }
-
     let favoriteButton = `<button class="fishing-favorite-item" data-inventory-id="${item.id}">Favorite</button>`;
     if (item.favorited === 1) {
       favoriteButton = `<button class="fishing-unfavorite-item" data-inventory-id="${item.id}">❤️</button>`;
     }
+    let caughtTime = null;
+    if (item.ctime) {
+      let date = new Date(item.ctime);
+      caughtTime = date.toLocaleString();
 
+      let now = new Date();
+
+      // time between now and caughtTime in time units
+      let timeDiff = now - date;
+      let timeUnits = Math.floor(timeDiff / 1000 / 60); // convert to minutes
+      
+      // formate caught time as "X minutes/hours/days ago"
+      if (timeUnits < 60) {
+        caughtTime = `${timeUnits} minutes ago`;
+      } else if (timeUnits < 60 * 24) {
+        caughtTime = `${Math.floor(timeUnits / 60)} hours ago`;
+      } else {
+        caughtTime = `${Math.floor(timeUnits / 60 / 24)} days ago`;
+      }      
+
+    }
+   
     console.log('Rendering fish inventory item:', item);
-    let metadata = item.metadata ? JSON.parse(item.metadata) : {};
+    let metadata = {};
+    if (item.metadata && typeof item.metadata === 'string') {
+      metadata = JSON.parse(item.metadata);
+    }
     item.value = metadata.value || item.value || 0;
+    this.totalValue += item.value;
+    let mutationStr = '';
+    if (metadata.mutation) {
+      mutationStr = `<br/>Mutation: ${metadata.mutation} <br/>`;
+    }
+
     return `<div class="fishing-item">
       <strong>${item.item_def.name}</strong><br/>
       Type: ${item.item_def.type}<br/>
+      Age: ${caughtTime || 'N/A'}<br/>
       Rarity: ${item.item_def.rarity}<br/>
       Value: ${item.value} coins<br/>
+      ${mutationStr}
       Description: ${item.item_def.description}<br/>
       <button class="fishing-sell-item" data-inventory-id="${item.id}">Sell</button>
       ${favoriteButton}
@@ -118,7 +150,11 @@ export default class Fishing {
     let fishInventory = await this.client.apiRequest('/inventory', 'GET');
     console.log('Player fishing inventory:', fishInventory);
 
+    this.totalValue = 0;
     let html = fishInventory.map(item => this.renderFishInventoryItem(item)).join('');
+    let formattedValue = this.totalValue.toLocaleString();
+    $('.fishing-total-inventory-value', this.win.content).text(formattedValue);
+
     $('.fishing-inventory-list', this.win.content).empty().append(html);
   }
 
@@ -132,13 +168,18 @@ export default class Fishing {
 
     let equippedHtml = `<h3>Equipped Items</h3>`;
     if (equipped) {
-
+      this.equippedItems = [];
       equipped.forEach(item => {
+        this.equippedItems.push(item.inventory_id);
+        // TODO: add back durability and charges display
+        // Durability: ${item.metadata.durability}<br/>
         equippedHtml += `<div class="fishing-item">
           <strong>${item.metadata.key}</strong><br/>
           Type: ${item.metadata.key}<br/>
           Rarity: ${item.metadata.rarity}<br/>
-          Durability: ${item.metadata.item_durability}<br/>
+          <em>${item.metadata.description}</em><br/>
+
+          <button class="fishing-unequip-item" data-inventory-id="${item.inventory_id}">Unequip</button>
         </div>`;
 
       });
@@ -151,12 +192,20 @@ export default class Fishing {
   async loadItems() {
     let items = await this.client.apiRequest('/items', 'GET');
     console.log('Player fishing items:', items);
-
+    console.log(' this.equippedItems.length',  this.equippedItems);
+    $('.fishing-items-list', this.win.content).empty();
     items.forEach(item => {
+      // check that item.inventory id is not in this.equippedItems
+      if (this.equippedItems.includes(item.id)) {
+        console.log('Skipping equipped item:', item);
+        return;
+      }
+
       let itemDef = JSON.parse(item.item_def);
       console.log('Item def:', itemDef);
+      console.log('renderFishItem', item)
       let itemHtml = this.renderFishItem(item);
-      $('.fishing-items', this.win.content).append(itemHtml);
+      $('.fishing-items-list', this.win.content).append(itemHtml);
     });
   }
 
@@ -197,6 +246,19 @@ export default class Fishing {
         // show a message
         let value = result.item.value || 0;
         $('.fishing-results', this.win.content).prepend(`<p>Sold item for ${value} coins.</p>`);
+
+        if (result.value && result.value > 0) {
+          console.log('Emitting reward event for sold item:', result);
+              bp.emit('buddylist-websocket::reward', {
+                success: true,
+                message: {
+                  newBalance: result.value, 
+                },
+                reward: result.reward,
+              });
+
+        }
+
       } else {
 
         console.log('Error selling item:', result);
@@ -233,8 +295,15 @@ export default class Fishing {
       const inventoryId = $(e.currentTarget).data('inventory-id');
       await this.equipItem(inventoryId);
       await this.loadEquipped();
+      await this.loadItems();
     });
-
+    // fishing-unequip-item
+    $('.fishing-app', this.win.content).on('click', '.fishing-unequip-item', async (e) => {
+      const inventoryId = $(e.currentTarget).data('inventory-id');
+      await this.client.apiRequest(`/unequip`, 'POST', { inventory_id: inventoryId });
+      await this.loadEquipped();
+      await this.loadItems();
+    });
 
   }
 
@@ -254,7 +323,7 @@ export default class Fishing {
       let resultHtml = `<p>You caught a <strong>${result.name}</strong> (Rarity: ${result.rarity})</p>`;
       resultHtml += `<p>Value: ${result.value} coins</p>`;
       if (result.mutation) {
-        resultHtml += `<p>Mutation occurred! You also caught a <strong>${result.mutation.name}</strong> (Rarity: ${result.mutation.rarity})</p>`;
+        resultHtml += `<p>Mutation occurred! You also caught a <strong>${result.mutation}</strong></p>`;
       }
       resultsDiv.prepend(resultHtml);
 
@@ -265,10 +334,13 @@ export default class Fishing {
       if (result.item_def.type === 'fish') {
         let itemHtml = this.renderFishInventoryItem(result);
         $('.fishing-inventory-list', this.win.content).prepend(itemHtml);
+        let formattedValue = this.totalValue.toLocaleString();
+        $('.fishing-total-inventory-value', this.win.content).text(formattedValue);
+
       }
       if (result.item_def.type === 'fishing-item') {
         let itemHtml = this.renderFishItem(result);
-        $('.fishing-items', this.win.content).prepend(itemHtml);
+        $('.fishing-items-list', this.win.content).prepend(itemHtml);
       }
 
     } catch (err) {
@@ -285,8 +357,9 @@ export default class Fishing {
 
 
     if (result.success) {
+      let formattedValue = result.totalValue.toLocaleString();
       let resultsDiv = $('.fishing-results', this.win.content).empty();
-      let resultHtml = `<p>Sold ${result.sold_count} items for a total of ${result.totalValue} coins.</p>`;
+      let resultHtml = `<p>Sold ${result.sold_count} items for a total of ${formattedValue} coins.</p>`;
       resultsDiv.prepend(resultHtml);
 
     }
@@ -298,5 +371,6 @@ export default class Fishing {
     }
 
     await this.renderFishInventory();
+    await this.loadItems();
   }
 }
