16448760380f376745dcd8e59846dcec0f598862
Marak <marak.squires@gmail.com>
2025-10-18 20:30:59 -1000

    Adds missing `data` cards

---
+++ v5/dist/apps/based/card/cards/data/data-card.js
@@ -0,0 +1,2 @@
+function n(n,s,t,e){const a=$(n);s.message.text="",a.find(".rag-result-json").val(JSON.stringify(s.json,null,2))}export{n as default};
+//# sourceMappingURL=data-card.js.map
---
+++ v5/dist/apps/based/card/cards/rag-result/rag-result-card.js
@@ -0,0 +1,2 @@
+function l(l,n,t,a){$(l).find(".rag-result-json").val(JSON.stringify(n.ragResult,null,2))}export{l as default};
+//# sourceMappingURL=rag-result-card.js.map
